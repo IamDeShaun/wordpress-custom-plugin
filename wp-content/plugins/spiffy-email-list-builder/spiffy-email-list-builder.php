9a2c062a7bf8b5135be1995fd657886b5f9f4a66
DeShaun Jones <viceprezofnv@gmail.com>
2018-09-13 15:14:00 -0400

    Added the header information for the plugin

---
+++ wp-content/plugins/spiffy-email-list-builder/spiffy-email-list-builder.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+/*
+Plugin Name: Spiffy Email List Builder
+Plugin URI: 
+Description: The ultimate email list building plugin for WordPress. Capture new subscribers. Reward subscribers with a custom download upon opt-in. Build unlimited lists. Import and export subscribers easily with .csv
+Version: 1.0
+Author: DeShaun Jones
+Author URI: http://deshaunjones.com 
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: spiffy-email-list-builder
+*/
