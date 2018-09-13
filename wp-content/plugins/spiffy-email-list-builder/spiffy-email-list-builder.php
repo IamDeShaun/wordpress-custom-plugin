bddd516a9f15ea0f8aaf2c3dd5b88a69447960d3
DeShaun Jones <viceprezofnv@gmail.com>
2018-09-13 15:21:53 -0400

    Added structure for codiing best practices.

--- wp-content/plugins/spiffy-email-list-builder/spiffy-email-list-builder.php
+++ wp-content/plugins/spiffy-email-list-builder/spiffy-email-list-builder.php
@@ -12,3 +12,83 @@ License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: spiffy-email-list-builder
 */
+
+
+/* !0. TABLE OF CONTENTS */
+
+/*
+	
+	1. HOOKS
+	
+	2. SHORTCODES
+		
+	3. FILTERS
+		
+	4. EXTERNAL SCRIPTS
+		
+	5. ACTIONS
+		
+	6. HELPERS
+		
+	7. CUSTOM POST TYPES
+	
+	8. ADMIN PAGES
+	
+	9. SETTINGS
+	
+	10. MISCELLANEOUS 
+
+*/
+
+
+
+
+/* !1. HOOKS */
+
+
+
+
+/* !2. SHORTCODES */
+
+
+
+
+/* !3. FILTERS */
+
+
+
+
+/* !4. EXTERNAL SCRIPTS */
+
+
+
+
+/* !5. ACTIONS */
+
+
+
+
+/* !6. HELPERS */
+
+
+
+
+/* !7. CUSTOM POST TYPES */
+
+
+
+
+/* !8. ADMIN PAGES */
+
+
+
+
+/* !9. SETTINGS */
+
+
+
+
+/* !10. MISCELLANEOUS */
+
+
+
